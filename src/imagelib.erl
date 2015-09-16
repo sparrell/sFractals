62cc23b4895dea47629d5a186948b8452082490a
sparrell <duncan@sfractal.com>
2015-09-15 23:31:39 -0400

    interim save
    still getting config to work

--- src/imagelib.erl
+++ src/imagelib.erl
@@ -10,20 +10,20 @@
 %% inspirations included ??
 
 %% API
--export([ makeImageFromData/4 ]).
+-export([ makeImageFromData/2, colorizeData/3 ]).
 %% need to add function to colorize iter data
  
-makeImageFromData( ColorData, Width, Height, FractalImageFileName ) ->
+makeImageFromData( ColorData, Config ) ->
     %% this function takes the x,y,color data and creates an image
 
     %% create a blank image object
-    FractalImage = egd:create(Width, Height),
+    FractalImage = egd:create( dict:find(width,Config), dict:find(height,Config)),
 
     %% call function to recurse thru list adding each point
     addAllData(FractalImage, ColorData),
     
     %%  render and save in file
-    egd:save(egd:render(FractalImage, png), FractalImageFileName),
+    egd:save(egd:render(FractalImage, png), dict:find(fractalImageFileName,Config)),
 
     %% clean up (is this needed?)
     egd:destroy(FractalImage).
@@ -41,3 +41,32 @@ addAllData( _FractalImage, [] ) ->
 addOnePoint(FractalImage, {X, Y, ColorValue}) ->
     %% this function adds one data point to image
     egd:line(FractalImage, {X, Y}, {X, Y}, ColorValue).
+
+% take count data and turn into colors based on color algorithm and parameters
+colorizeData(CountData,ColorAlg, Parameters) 
+    when ColorAlg == simplest ->
+    %% simplest alg is counts 0 thru 10 map to 11 colors (all others white)
+    colorizeData(CountData,ColorAlg, Parameters, []).  %% accumulator is ColorData
+
+colorizeData([],_ColorAlg, _Parameters, ColorData) -> % CountData empty so work is done
+    ColorData;
+colorizeData(CountData,ColorAlg, Parameters,ColorData) 
+        when ColorAlg == simplest ->
+        %% simplest alg is counts 0 thru 10 map to 11 colors (all others white)
+    [ {X,Y,Count} | NewCountData ] = CountData,    %get point at head of list
+    % convert Count to Color
+    Color = case Count of
+        0  -> egd:color({255,255,255});   % white
+        1  -> egd:color({0,255,255});   %cyan
+        2  -> egd:color({0,0,255});  % blue
+        3  -> egd:color({255,0,0});  % red
+        4  -> egd:color({0,255,0});  % green
+        5  -> egd:color({255,255,0}); % yellow
+        6  -> egd:color({210,180,140}); %tan
+        7  -> egd:color({240,128,128}); %lightcoral
+        8  -> egd:color({255,165,0}); %orange
+        9  -> egd:color({128,128,0}); %olive
+        10 -> egd:color({255,0,255}); %fuschia
+        _  -> egd:color({0,0,0})  %%everything else black
+    end,
+    colorizeData(NewCountData,ColorAlg, Parameters,[ {X,Y,Color} | ColorData]).
--- src/simpleFractal.erl
+++ src/simpleFractal.erl
@@ -9,24 +9,41 @@
 %% try to make simple fractal data and then turn into color data and then make image
 
 %% API
--export([ simplFrac/1 ]).
+-export([ simplFrac/1,testConfig1/0,testOne/0 ]).
  
+%% test config 1 - eventually make parameterized and move this to test dir
+testConfig1() ->
+    % return dict of config parameters
+
+    %% put list of key/values into dict and return it
+    dict:from_list( [ {fractalAlg, julian},  % Fractal Algorithm is julian
+      { fractalImageFileName, "./julian10.10.dot5.dot5.4.100.png"},  %image file created
+      { width, 10}, % width=10
+      { height, 10}, % height=10
+      { cReal, 0.5}, % real portion of C0
+      { cImaginary, -0.5}, % imaginary portion of C0
+      { zReal, -0.1}, %real portion of Z0 (don't care for Julian)
+      { zImaginary, -0.1}, %imaginary portion of Z0 (don't care for Julian)
+      { xRealRight, 3.0}, 
+      { xRealLeft, -3.0},
+      { yImaginaryLow, -3.0},
+      { yImaginaryHigh, 3.0},
+      { bailoutThreshold, 4},
+      { maxIterationThreshold, 100} ] ).
+
+    
+%% test1 - first simple fractal
+testOne() ->
+    simplFrac(testConfig1()).
+
 
 
 % create a simple fractal
-simplFrac(_) ->
+simplFrac(Config) ->
 
     %% FractalConfig = {FractalAlg,CReal,CImaginary, ZReal,ZImaginary,BailoutThreshold,MaxIterationThreshold}
     %% simple config
     FractalConfig = {julian,0.5,-0.5, 0,0,4,100},
-        %% FractalAlg = julian
-        %% CReal = 0.5
-        %% CImaginary = -0.5
-        %% ZReal = 0 (don't care for Julian)
-        %% ZImaginary = 0 (don't care for Julian)
-        %% BailoutThreshold = 4
-        %% MaxIterationThreshold = 100
-    FractalImageFileName = "./myFirstFractal.png",
     Width = 10,
     Height = 10,
 
@@ -35,22 +52,20 @@ simplFrac(_) ->
         %% XRealLeft = -3.0
         %% YImaginaryLow = -3.0
         %% YImaginaryHigh = 3.0
-    IterationCountData = createPointData(Width,Height,-3.0, 3.0, -3.0, 3.0, FractalConfig),
+    IterationCountData = createPointData(Width,Height,-3.0, 3.0, -3.0, 3.0, FractalConfig,Config),
 
     %% print some stats on distribution of counts
     %% need to do
 
-    %% colorize the data
-%%  OtherParameters = {0,0},
-%%  ColorData = imagelib:colorizeData(IterationCountData,OtherParameters),
+    %% colorize the data using simplest alg
+    ColorData = imagelib:colorizeData(IterationCountData,simplest,{}),
 
     %% take the x,y,color data and creates an image
-%%  imagelib:makeImageFromData( ColorData, Width, Height, FractalImageFileName ),
+    imagelib:makeImageFromData( ColorData, Config ),
 
-%%  ok.
-    IterationCountData.
+    ok.
 
-createPointData(Width,Height, XRealLeft, XRealRight, YImaginaryLow, YImaginaryHigh, FractalConfig) 
+createPointData(Width,Height, XRealLeft, XRealRight, YImaginaryLow, YImaginaryHigh, FractalConfig,Config) 
     when XRealLeft < XRealRight, YImaginaryLow < YImaginaryHigh ->
     %% box is bounded on left by x > XRealLeft and bounded on right by x < XRealRight
     %% box is bounded on top by y > YImaginaryHigh and bounded on bottom by y > YImaginaryLow
@@ -60,7 +75,7 @@ createPointData(Width,Height, XRealLeft, XRealRight, YImaginaryLow, YImaginaryHi
     DeltaX = (XRealRight - XRealLeft) / Width,
     DeltaY = (YImaginaryHigh - YImaginaryLow) / Height,
 
-    makeAllRows(Width,Height,1,XRealLeft,YImaginaryLow,DeltaX,DeltaY,FractalConfig,[]).
+    makeAllRows(Width,Height,1,XRealLeft,YImaginaryLow,DeltaX,DeltaY,FractalConfig,[],Config).
         %% note the 3rd parameter is "CurrentPixelY" which starts at bottom
         %% note the 9th parameter is "IterCounts" which starts as empty list
 		%% makeAllRows returns InterCount, which is then returned by createPointData 
@@ -70,45 +85,69 @@ createPointData(Width,Height, XRealLeft, XRealRight, YImaginaryLow, YImaginaryHi
 %%  which is useful in case later we might to build file on fly that way 'from top'
 
 % function head for done
-makeAllRows(_Width,Height, CurrentPixelY,_XRealLeft,_CurrentImaginaryY,_DeltaX,_DeltaY,_FractalConfig,IterCounts)
+makeAllRows(_Width,Height, CurrentPixelY,_XRealLeft,_CurrentImaginaryY,_DeltaX,_DeltaY,_FractalConfig,IterCounts,Config)
         when CurrentPixelY > Height ->  
     IterCounts;   %reached bottom so return the iteration count data
 
 % function head for not-done so recurse
-makeAllRows(Width,Height,CurrentPixelY,XRealLeft,CurrentImaginaryY,DeltaX,DeltaY,FractalConfig,IterCounts) ->
+makeAllRows(Width,Height,CurrentPixelY,XRealLeft,CurrentImaginaryY,DeltaX,DeltaY,FractalConfig,IterCounts,Config) ->
     % reached here so need to make another row
     % note Y-1 one since doing top down
     % similarly Imaginary& went down by delta
 	% IterCounts is updated with the new row calculated by makePoints and is the returned value
     makeAllRows(Width,Height,CurrentPixelY+1,XRealLeft,CurrentImaginaryY+DeltaY,DeltaX,DeltaY,FractalConfig,
-        makePoints(Width,1,CurrentPixelY,XRealLeft, CurrentImaginaryY,DeltaX,FractalConfig,IterCounts) ).
+        makePoints(Width,1,CurrentPixelY,XRealLeft, CurrentImaginaryY,DeltaX,FractalConfig,IterCounts,Config),Config ).
     
 % function head for done-with-row
-makePoints(Width,CurrentPixelX,_CurrentPixelY,_CurrentRealX,_CurrentImaginaryY,_DeltaX,_FractalConfig,IterCounts)
+makePoints(Width,CurrentPixelX,_CurrentPixelY,_CurrentRealX,_CurrentImaginaryY,_DeltaX,_FractalConfig,IterCounts,Config)
         when CurrentPixelX > Width ->  
     IterCounts;   %reached end of line so return the iteration count data
 
 % function head for not-done so recurse
-makePoints(Width,CurrentPixelX,CurrentPixelY,CurrentRealX,CurrentImaginaryY,DeltaX,FractalConfig,IterCounts) ->   
+makePoints(Width,CurrentPixelX,CurrentPixelY,CurrentRealX,CurrentImaginaryY,DeltaX,FractalConfig,IterCounts,Config) ->   
     %reached here so need to add another pixel
     % CurrentPixelX starts at 1 increases by 1 each recurse
     % CurrentRealX starts at XRealLeft and increases by DeltaX each recurse
     % IterCounts has point {X,Y,Iter} added by function addOnePoint
+    {ok, FractalAlg} = dict:find(fractalAlg,Config),
     makePoints(Width,CurrentPixelX+1,CurrentPixelY,CurrentRealX+DeltaX,CurrentImaginaryY,DeltaX,FractalConfig,
-        addOnePoint(CurrentPixelX,CurrentPixelY,CurrentRealX,CurrentImaginaryY,FractalConfig,IterCounts) ).
+        addOnePoint(CurrentPixelX,
+                    CurrentPixelY,
+                    CurrentRealX,
+                    CurrentImaginaryY,
+                    FractalAlg,
+                    IterCounts,
+                    Config),
+         Config ).
         %% note addOnePoint returns the new value of IterCounts into the recursive call
 
 %% Compute fractal (bounded or unbounded)
     %% FractalConfig = {FractalAlg,CReal,CImaginary, ZReal,ZImaginary,BailoutThreshold,MaxIterationThreshold}
     %% FractalAlg is which algorithm (only one at moment)
 % this function clause for creating julian fractals. eventually will generalize
-addOnePoint(CurrentPixelX,CurrentPixelY,Xreal,Yimaginary,{FractalAlg,CReal,CImaginary, ZReal,ZImaginary,BailoutThreshold,MaxIterationThreshold},IterCounts) 
+addOnePoint(CurrentPixelX,
+            CurrentPixelY,
+            Xreal,
+            Yimaginary,
+            FractalAlg,
+            IterCounts,
+            Config) 
     when FractalAlg == julian ->
 
+    { ok, CReal } = dict:find(cReal,Config),
+    { ok, CImaginary } = dict:find(cImaginary,Config),
+    { ok, MaxIterationThreshold } = dict:find(maxIterationThreshold,Config),
+    { ok, BailoutThreshold } = dict:find(bailoutThreshold,Config),
     %% return IterCounts with new point added. Note 3rd item in tuple is Count, and 4th item in compute call is initial iteration
-    [ {CurrentPixelX, CurrentPixelY,
-        computeIterationValue(CReal,CImaginary,Xreal,Yimaginary,
-            0, MaxIterationThreshold, BailoutThreshold ) } | IterCounts ].
+    Count = computeIterationValue(CReal,
+                                  CImaginary,
+                                  Xreal,
+                                  Yimaginary,
+                                  0,                     % initial iteration count
+                                  MaxIterationThreshold, 
+                                  BailoutThreshold ),
+
+    [ {CurrentPixelX, CurrentPixelY, Count } | IterCounts ].
 
 %% computeIterationValue computes fractal value and returns iteration count
 %% function clause for exceeding iteration count
