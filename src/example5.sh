6ffdc3ca07e4f532ce5483f88ff68823009ba36d
sparrell <duncan@sfractal.com>
2015-10-04 22:59:30 -0400

    new palettes

---
+++ src/example5.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -pa ../_build/test/lib/sFractals/ebin ../_build/test/lib/png/ebin
+%% assumes running out of _build
+
+%%%-------------------------------------------------------------------
+%%% @author Duncan Sparrell
+%%% @copyright (C) 2015, sFractal Consulting LLC
+%%% 
+%%%-------------------------------------------------------------------
+
+-author("Duncan Sparrell").
+-mode(compile).
+
+main(_) ->
+
+    % example 5
+
+    %% base config
+    ConfigMap = #{ fractalAlg => julian,  % Fractal Algorithm is julian
+                    fractalImageFileName => "test/example5.png",  %image file created
+                    colorAlg => simple32,  % 0-31 map to colors
+                    width => 4000,           % width=4k
+                    height => 4000,          % height=4k
+                    cReal => 0.5,          % real portion of C0
+                    cImaginary => -0.5,    % imaginary portion of C0
+                    zReal => -0.1,         % real portion of Z0 (na Julian)
+                    zImaginary => -0.1,    % imaginary portion of Z0 (na Julian)
+                    xRealRight => 1.0,
+                    xRealLeft => -1.0,
+                    yImaginaryLow => 0.0,
+                    yImaginaryHigh => 2.0,
+                    bailoutThreshold => 4,
+                    maxIterationThreshold => 31 },
+
+    %% uncomment %~ to give staus io
+    %~statistics(runtime),
+    %~statistics(wall_clock),
+
+    % create the data
+    %~io:format("computing data~n"),
+    Rows = simpleFractal:computeFractalData( ConfigMap ),
+
+    %% make image from data
+    %~io:format("rendering image~n"),
+    simpleFractal:makePngFromData(Rows,ConfigMap),
+
+    %~{_, TimeRun} = statistics(runtime),
+    %~{_, TimeWall} = statistics(wall_clock),
+    %~SecRun = TimeRun / 1000.0,
+    %~SecWall = TimeWall / 1000.0,
+    %~io:format("    ~p runtime seconds~n    ~p wall clock seconds~n",
+    %~          [SecRun, SecWall]),
+
+    ok.
+
