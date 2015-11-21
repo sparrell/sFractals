d4125b14df35e6f136e35f2d7642b942b348029f
sparrell <duncan@sfractal.com>
2015-11-20 21:36:28 -0500

    iss 29 remove crud
    took out unused functions in helpers - but still need to clean up
    simple fractal

--- src/fractalHelpers.erl
+++ src/fractalHelpers.erl
@@ -10,7 +10,6 @@
 
 %% public API
 -export([ 
-          computeFractalData/1,           % create a block of fractal data
           computeRowOfFractalData/4,      % create one row of fractal data
           computeAllRowsOfFractalData/1      % create one row of fractal data
           ]).
@@ -18,8 +17,8 @@
 %% expose functions for test
 -export([ computeXList/1,computeYList/1,
           newImaginaryC/1,newRealC/1,newImaginaryZ/1,newRealZ/1,
-          computeIterationValue/8, 
-          computeFractalData/11 ]).
+          computeIterationValue/8 
+          ]).
  
 %%%%%%%%
 %% computeAllRowsOfFractalData/1 API
@@ -35,115 +34,6 @@ computeAllRowsOfFractalData(ConfigMap) ->
     FractalData = [ {{PixelY,ImgY}, computeRowOfFractalData(FractalAlg, {PixelY,ImgY},XList,ConfigMap)} || 
                         {PixelY,ImgY} <- YList ],
     FractalData.
-
-%%%%%%%%
-%% computeFractalData/1 API
-%%        ConfigMap    - config info
-%%        returns Rows (list of rows where each row is list of counts, 1 per pixel)
-%%%%%%%%
-computeFractalData(ConfigMap) ->
-    %% get config needed
-    %% create height rows of width columns of pixels
-    Width           = maps:get(width,ConfigMap),
-    Height          = maps:get(height,ConfigMap),
-    %% each pixel has a corresonding complex number defined by corners of the box
-    XRealRight      = maps:get(xRealRight,ConfigMap),
-    XRealLeft       = maps:get(xRealLeft,ConfigMap),
-    YImaginaryLow   = maps:get(yImaginaryLow,ConfigMap),
-    YImaginaryHigh  = maps:get(yImaginaryHigh,ConfigMap),
-    %% box is bounded on left by x > XRealLeft and bounded on right by x < XRealRight
-    %% box is bounded on top by y > YImaginaryHigh and bounded on bottom by y > YImaginaryLow
-    %% box is width pixels wide and height pixels high
-
-    %% step is floating range divided by number of pixels
-    DeltaX = (XRealRight - XRealLeft) / Width,
-    DeltaY = (YImaginaryHigh - YImaginaryLow) / Height,
-
-    %% call computeFractalData/11
-    % return Rows from computeFractalData/11
-    computeFractalData( [],             % Rows starts empty
-                        [],             % ThisRow starts empty
-                        Width,          % XPix starts width and decrements to 1
-                        XRealRight,     %% XR starts at right and decrements to by deltaX
-                        DeltaX,         
-                        Width,          % width
-                        Height,         % YPix starts height and decrements to 1
-                        YImaginaryHigh, % YI starts at top and decrements by delta y
-                        DeltaY,         
-                        Height,         % height
-                        ConfigMap).    % map of parameters
-
-
-
-%%%%%%%%
-%% computeFractalData/11 API
-%%        Rows         - a list of the rows(lines) of data, starts empty and builds until height reached, built top down
-%%        ThisRow      - a list of the points(count value) in a row/line, starts empty and builds R->L until width reached
-%%        XPix         - the integer X value of the pixel 
-%%        XR           - the real component of the floating point number for computing fractal for this XPix
-%%        DeltaX       - for each pixel, XR increases by this amount
-%%        Width        - width of fractal in pixels
-%%        YPix         - the integer Y value of the pixel
-%%        YI           - the imaginary component of the floating point number for computing fractal for this YPix
-%%        DeltaY       - for each pixel, YI increases by this amount
-%%        Height       - height(intger) of image = number of rows
-%%        ConfigMap    - config info
-%%%%%%%%
-% clause when height is reached, return the rows of data
-computeFractalData( Rows, _ThisRow,
-               _XPix, _XR, _DeltaX, _Width,  
-               YPix, _YI, _DeltaY, _Height,  % only height matters
-               _ConfigMap)
-        when YPix =< 0 ->
-               
-    %% pixels all made already so done
-    Rows;
-
-% clause when row is complete  but height not reached - process row and recurse
-computeFractalData( Rows, ThisRow,        % row data computed so far
-               XPix, _XR, DeltaX, Width,  % info for points in a row
-               YPix, YI, DeltaY, Height,  % info for rows
-               ConfigMap)
-        when XPix =< 0, YPix >= 0 ->
-
-    % add row
-    NewRows = [ ThisRow | Rows ],
-
-    % reset to begining of next row 
-    NewRowData = [],                                 % reset data for row to empty
-    NewXPix    = Width,                              % reset to end of line
-    NewXR      = maps:get(xRealRight,ConfigMap),     % reset to end of line
-    NewYPix    = YPix - 1,                           % increment row
-    NewYI      = YI - DeltaY,                          % increment row
-    computeFractalData( NewRows, NewRowData,
-                   NewXPix, NewXR, DeltaX, Width,
-                   NewYPix, NewYI, DeltaY,Height,
-                   ConfigMap);
-
-computeFractalData( Rows, RowData,       % row data computed so far
-               XPix, XR, DeltaX, Width,   % info for points in a row
-               YPix, YI, DeltaY, Height,  % info for rows
-               ConfigMap)
-        when XPix > 0, YPix > 0 ->
-
-    %% get iteration count for this point
-    NewPoint = computeIterationValue( maps:get(fractalAlg,ConfigMap),
-                                      maps:get(cReal,ConfigMap),
-                                      maps:get(cImaginary,ConfigMap),
-                                      XR,
-                                      YI,
-                                      0,          %iteration count starts at zero
-                                      maps:get(maxIterationThreshold,ConfigMap),
-                                      maps:get(bailoutThreshold,ConfigMap)
-                                      ),
-                           
-    NewRowData = [ NewPoint | RowData ],
-    NewXPix    = XPix - 1,                           % decrement moving left building row
-    NewXR      = XR - DeltaX,                        % decrease XR to the left
-    computeFractalData( Rows, NewRowData,
-                   NewXPix, NewXR, DeltaX, Width,
-                   YPix, YI, DeltaY,Height,
-                   ConfigMap).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %% computeRowOfFractalData computes one row of fractal data
