81269482b672eab8be8acb1df49a538e5ec425b9
sparrell <duncan@sfractal.com>
2015-12-09 20:47:56 -0500

    interim - cowboy running but not handling

--- src/sFractals_app.erl
+++ src/sFractals_app.erl
@@ -17,7 +17,9 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    'sFractals_sup':start_link().
+    {ok, Sf_Pid} = 'sFractals_sup':start_link(),
+    start_webserver(),
+    {ok, Sf_Pid}.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
@@ -29,3 +31,31 @@ start() ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+start_webserver() ->
+    lager:info("cowboy webserver about to start"),
+    Port = 8080,  % get this from config when clean this up
+    ListenerCount = 5, % how many parralell listeners (don't need many)
+    MaxConnections = 100, % how many can use at same tim
+    StaticPages = [ { directory, {priv_dir,sFractals,[]} }
+                  , {mimetypes, [{<<".html">>, [<<"text/html">>]}]}
+                  ],
+    Routes = [
+               {
+                 '_'  %virtual hostname (any host name)
+               , [ {<<"/status">>, status_handler, []}
+                 , {<<"/[...]">>, cowboy_static, StaticPages }
+                 ]
+               }
+             ],
+    Dispatch = cowboy_router:compile(Routes),
+    {ok, _} = cowboy:start_http( http
+                               , ListenerCount
+                               , [{port, Port}]
+                               , [ {env, [{dispatch, Dispatch}]} ]
+                               ),
+    ok.
+
+
+
+
